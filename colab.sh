f463ff57b5d457d08418d0a8961748a4f4313dcb
sangeet <sangeet.kar@gmail.com>
2020-05-12 10:34:12 +0000

    first commit

---
+++ colab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+driveFolder="/content/drive/My Drive/ml"
+dest="/content/"
+
+echo "$driveFolder$1"
+
+if [ ! -e "$dest$1" ] 
+then
+        if [ ! -e "$driveFolder$1" ]; then
+                mkdir -p "$driveFolder$1/input"
+                mkdir "$driveFolder$1/models"
+        fi
+        ln -s "$driveFolder$1" "$dest"
+else
+    echo  already exists..
+fi
+
